afb92bc4066557562382ea9bae32322f2e3bd66c
FallenFlame5567 <mattruff78@gmail.com>
2020-01-20 16:25:03 -0500

    Storing vision testing changes

--- Robot/src/main/java/frc/robot/Robot.java
+++ Robot/src/main/java/frc/robot/Robot.java
@@ -12,8 +12,14 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import java.util.Map;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 /**
@@ -46,6 +52,13 @@ public class Robot extends TimedRobot {
 
     NetworkTable limelightTable;
 
+    //declare private variables for creating a tab, instantiating a launcher and retrieving data
+    private ShuffleboardTab cameraTab;
+    private NetworkTableEntry cameraHeight;
+    private NetworkTableEntry cameraAngle;
+    private NetworkTableEntry targetHeight;
+    private NetworkTableEntry distance;
+
     public Robot() {
         leftTalon = new TalonSRX(1);
         rightTalon = new TalonSRX(2);
@@ -62,6 +75,26 @@ public class Robot extends TimedRobot {
 
         limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
         isDriverCamera = false;
+
+        cameraTab = Shuffleboard.getTab("Camera");
+
+        cameraHeight = cameraTab.addPersistent("Camera Height (m)", 0.0)                
+                              .withWidget(BuiltInWidgets.kTextView)             
+                              .withProperties(Map.of("min", 0.0, "max", 6.0)) 
+                              .getEntry();
+
+        cameraAngle = cameraTab.addPersistent("Camera Angle (deg)", 0.0)                
+                              .withWidget(BuiltInWidgets.kTextView)             
+                              .withProperties(Map.of("min", 0.0, "max", 90.0)) 
+                              .getEntry();
+        
+        targetHeight = cameraTab.addPersistent("Target Height (m)", 0.0)                
+                              .withWidget(BuiltInWidgets.kTextView)             
+                              .withProperties(Map.of("min", 0.0, "max", 6.0)) 
+                              .getEntry();
+
+        distance = cameraTab.addPersistent("Distance", 0.0)
+                              .getEntry();
 
     }
 
@@ -96,21 +129,30 @@ public class Robot extends TimedRobot {
 
     @Override
     public void testPeriodic() {
-        if(testController.getAButton()) {
-            shooterControl.setSpeed();
-        }
-        else {
-            shooterControl.zeroSpeed();
-        }
+        // if(testController.getAButton()) {
+        //     shooterControl.setSpeed();
+        // }
+        // else {
+        //     shooterControl.zeroSpeed();
+        // }
 
         if(testController.getBButtonReleased()) {
             if(isDriverCamera) {
                 limelightTable.getEntry("camMode").setNumber(0);
+                limelightTable.getEntry("ledMode").setNumber(0);
             }
             else {
-                limelightTable.getEntry("camMode").setNumber(0);
+                limelightTable.getEntry("camMode").setNumber(1);
+                limelightTable.getEntry("ledMode").setNumber(1);
             }
+            isDriverCamera = !isDriverCamera;
         }
+
+        distance.setDouble((targetHeight.getDouble(0) - cameraHeight.getDouble(0)) / Math.tan( cameraAngle.getDouble(0) + limelightTable.getEntry("ty").getDouble(0)));
+
+
+
+
     }
 
 }
