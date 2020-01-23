72574f5d91cca51bfb5a00b9387a54ae3a06d602
--replace-all <josh.overbeek@outlook.com>
2020-01-23 17:54:45 -0500

    Changed a constant to final

--- Robot/src/main/java/frc/robot/RobotMap.java
+++ Robot/src/main/java/frc/robot/RobotMap.java
@@ -2,5 +2,5 @@ package frc.robot;
 
 public class RobotMap {
     //the time it takes the drive train to ramp to full speed in open loop control in seconds
-    public static double DRIVE_RAMP_TIME = 0.5;
+    public static final double DRIVE_RAMP_TIME = 0.5;
 }
