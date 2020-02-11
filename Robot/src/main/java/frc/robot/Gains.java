5f2a1b0f91f3eb0612f49548e8c53d406d6f75d8
HannahStrimpel <h4rescue@gmail.com>
2020-02-11 18:51:22 -0500

    Comments were added to the Gains class.

--- Robot/src/main/java/frc/robot/Gains.java
+++ Robot/src/main/java/frc/robot/Gains.java
@@ -1,7 +1,11 @@
+/**
+ * Class that organizes gains used when assigning values to slots
+ */
+
 package frc.robot;
 
 public class Gains {
-    //Declares the 
+    //Declares the variables to be passed into the gains function
     public final double kP;
     public final double kI;
     public final double kD;
@@ -9,7 +13,7 @@ public class Gains {
     public final int kIzone;
     public final double kPeakOutput;
 
-
+    //The function that organizes the PIDF variables
     public Gains(double _kP, double _kI, double _kD, double _kF, int _kIzone, double _kPeakOutput) {
         kP = _kP;
         kI = _kI;
