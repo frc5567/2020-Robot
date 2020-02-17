3ff0143c240b370800c22fc0e32fe506ed417901
--replace-all <josh.overbeek@outlook.com>
2020-02-17 17:02:59 -0500

    Updated documentation on Gains class

--- Robot/src/main/java/frc/robot/Gains.java
+++ Robot/src/main/java/frc/robot/Gains.java
@@ -1,9 +1,8 @@
-/**
- * Class that organizes gains used when assigning values to slots
- */
-
 package frc.robot;
 
+/**
+ * Class for storing PID constants and gains in the robot map
+ */
 public class Gains {
     //Declares the variables to be passed into the gains function
     public final double kP;
@@ -13,7 +12,16 @@ public class Gains {
     public final int kIzone;
     public final double kPeakOutput;
 
-    //The function that organizes the PIDF variables
+    /**
+     * Constructor for Gains objects.
+     * These objects store PID constants for easier reading in the RobotMap
+     * @param _kP The proportionality constant
+     * @param _kI The integral constant
+     * @param _kD The derivative constant
+     * @param _kF The feed-forward constant
+     * @param _kIzone The Integral zone -> if absolute closed loop error exceeds this value, error is reset
+     * @param _kPeakOutput The peak output of the PID control
+     */
     public Gains(double _kP, double _kI, double _kD, double _kF, int _kIzone, double _kPeakOutput) {
         kP = _kP;
         kI = _kI;
