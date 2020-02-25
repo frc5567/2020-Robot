e4d1df44b3e11726592cb5c8c372d20806beaa27
--replace-all <josh.overbeek@outlook.com>
2020-02-25 18:23:00 -0500

    Updated a comment

--- Robot/src/main/java/frc/robot/Intake.java
+++ Robot/src/main/java/frc/robot/Intake.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PWMSparkMax;
@@ -121,8 +122,9 @@ public class Intake {
      * Uses robot map constants for instantation
      */
     public Intake() {
-        //this is currently set to PWM to reduce can bus traffic
+        //the spark is currently set to operate over PWM to reduce can bus traffic
         m_outerMotor = new PWMSparkMax(RobotMap.INTAKE_PWM_SPARK_PORT);
+        
         m_innerMotor = new VictorSPX(RobotMap.INTAKE_VICTOR_ID);
 
         m_positionPiston = new DoubleSolenoid(RobotMap.INTAKE_POSITION_PISTON_FORWARD_PORT, RobotMap.INTAKE_POSITION_PISTON_REVERSE_PORT);
