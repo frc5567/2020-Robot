ef71d3f779b38884eecb145d99ab054279519c90
--replace-all <josh.overbeek@outlook.com>
2020-04-03 14:03:05 -0400

    Added a set to brake at the start of teleop

--- Robot/src/main/java/frc/robot/Robot.java
+++ Robot/src/main/java/frc/robot/Robot.java
@@ -98,6 +98,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
+        m_pilotController.getDrivetrain().setNeutralMode(NeutralMode.Brake);
     }
         
     @Override
